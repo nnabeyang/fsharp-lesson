49f3ce9ccdc4673c07927b138b40d73ca541cb74
nnabeyang <nabeyang@gmail.com>
2022-08-17 21:40:31 +0900

    課題5

--- sources/play_library/PlayDeedle/Scratch.fsx
+++ sources/play_library/PlayDeedle/Scratch.fsx
@@ -9,3 +9,10 @@ df.RowsDense
 |> Frame.ofRows
 // 課題4
 df.Columns.[ [|"場所";"学年"|] ]
+// 課題5
+let filter (pred: ObjectSeries<string> -> bool) (df: Frame<int,string>) = 
+  df.RowsDense
+  |> Series.filterValues pred
+  |> Frame.ofRows
+
+let project (cols: string[]) (df: Frame<int,string>) = df.Columns.[ cols ] 
