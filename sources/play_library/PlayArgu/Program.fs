b4f98c60a17e58e95ad23d336e6aaa95c4dff7f1
nnabeyang <nabeyang@gmail.com>
2022-08-17 21:57:04 +0900

    課題6-1

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,2 +1,9 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System
+
+// 課題6-1: Environment.GetCommandLineArgs()を使う方法
+let args = Environment.GetCommandLineArgs()
+match Array.contains "-hello" args with
+  | true -> printfn "Hello World"
+  | false -> printfn "I don't know"
+
