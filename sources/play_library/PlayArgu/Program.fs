3b287b7bbf721c280fba5d717bd7f3ac59ef45f1
nnabeyang <nabeyang@gmail.com>
2022-08-17 22:19:26 +0900

    課題7

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,10 +1,30 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-open System
+open Argu
 
-// 課題6-2: [<EntryPoint>]を使う方法
+// 課題7
+[<CliPrefix(CliPrefix.Dash)>]
+type Arguments =
+    | Hello
+
+    interface IArgParserTemplate with
+      member s.Usage =
+        match s with
+        | Hello -> "print Hello World"
+
 [<EntryPoint>]
 let main args =
-  match Array.contains "-hello" args with
-    | true -> printfn "Hello World"
-    | false -> printfn "I don't know"
+  let parser = ArgumentParser.Create<Arguments>(programName="課題7")
+  try
+    let rs = parser.Parse args
+    if rs.Contains Hello then
+      printfn "Hello World"
+    else
+      printfn "I don't know" 
+  with
+    | :? ArguParseException as ex ->
+      if ex.ErrorCode = ErrorCode.HelpText then
+        printfn "%s" (parser.PrintUsage())
+      else
+        printfn "I don't know"
+    | ex -> printfn "%s" ex.Message
   0
