83ed5fdbc0df49be8093db81d10b15b7a0a66581
nnabeyang <nabeyang@gmail.com>
2022-08-18 22:20:03 +0900

    課題8

--- sources/play_library/PlayArgu/Program.fs
+++ sources/play_library/PlayArgu/Program.fs
@@ -1,25 +1,39 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 open Argu
+open Deedle
 
-// 課題7
+// 課題8
 [<CliPrefix(CliPrefix.Dash)>]
 type Arguments =
-    | Hello
+    | Filter of col:string * value:string
+    | Project of cols:string list
 
     interface IArgParserTemplate with
       member s.Usage =
         match s with
-        | Hello -> "print Hello World"
+        | Filter _ -> "filter data frame"
+        | Project _ -> "project data frame"
+
+let filter (pred: ObjectSeries<string> -> bool) (df: Frame<int,string>) = 
+  df.RowsDense
+  |> Series.filterValues pred
+  |> Frame.ofRows
+
+let project (cols: list<string>) (df: Frame<int,string>) = df.Columns.[ cols ] 
 
 [<EntryPoint>]
 let main args =
-  let parser = ArgumentParser.Create<Arguments>(programName="課題7")
+  let df = Frame.ReadCsv "../../data/シラバス.csv"
+  let parser = ArgumentParser.Create<Arguments>(programName="課題8")
   try
-    let rs = parser.Parse args
-    if rs.Contains Hello then
-      printfn "Hello World"
-    else
-      printfn "I don't know" 
+    match parser.Parse args with
+      | p when p.Contains Filter ->
+        let (col, value) = p.GetResult Filter
+        (filter (fun row -> row.Get(col) = value) df).Print()
+      | p when p.Contains Project -> 
+         let cols = p.GetResult Project
+         (project cols df).Print()
+      | _ -> printfn "I don't know"
   with
     | :? ArguParseException as ex ->
       if ex.ErrorCode = ErrorCode.HelpText then
