ac3299fab3a88094ac6bb82b85923a4bb0c8a268
nnabeyang <nabeyang@gmail.com>
2022-08-20 12:00:48 +0900

    課題12

--- sources/play_library/PlayFParsec/Scratch.fsx
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -25,7 +25,9 @@ test pidentifier "i18n"
 test pidentifier "12"
 
 // 課題11
-type Project = Project of string list
+type Func = 
+  | Project of string list
+  | Filter of col:string * value:string
 let pProject =
   let ws = spaces
   let str_ws s = pstring s .>> ws
@@ -35,3 +37,19 @@ let pProject =
 
 test pProject "project([場所],  [学年])"
 test pProject "project ([場所][学年]"
+
+// 課題12
+let pFilter =
+  let ws = spaces
+  let str_ws s = pstring s .>> ws
+  let pColName =
+    let normalChar = satisfy (fun c -> c <> ']')    
+    between (pstring "[") (pstring "]") (many1Chars normalChar)
+  let pColValue =
+    let normalChar = satisfy (fun c -> c <> '\"')    
+    between (pstring "\"") (pstring "\"") (many1Chars normalChar)  
+  let pBody = pipe2 (pColName .>> ws) (pstring "=" >>. ws >>. pColValue) (fun x  z -> Filter (x, z))
+  str_ws "filter" >>. str_ws "(" >>.  pBody .>> str_ws ")"
+
+test pFilter "filter([専門] = \"数学\")"
+test pFilter "filter([専門] = \"数学\""
