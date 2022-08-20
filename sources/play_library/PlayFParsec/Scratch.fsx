6731cef5d714e13b733c28cc25628a63b2e51ddf
nnabeyang <nabeyang@gmail.com>
2022-08-20 12:51:12 +0900

    課題13

--- sources/play_library/PlayFParsec/Scratch.fsx
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -25,20 +25,20 @@ test pidentifier "i18n"
 test pidentifier "12"
 
 // 課題11
-type Func = 
-  | Project of string list
-  | Filter of col:string * value:string
+type Expression = 
+  | ProjectExpression of string list
+  | FilterExpression of col:string * value:string
 let pProject =
   let ws = spaces
   let str_ws s = pstring s .>> ws
   let column_ws = pColumn .>> ws
   let pColumns = str_ws "(" >>. sepBy column_ws (str_ws ",") .>> str_ws ")"
-  str_ws "project" >>. pColumns |>> Project .>> ws
+  str_ws "project" >>. pColumns |>> ProjectExpression .>> ws
 
 test pProject "project([場所],  [学年])"
 test pProject "project ([場所][学年]"
 
-// 課題12
+// 課題13
 let pFilter =
   let ws = spaces
   let str_ws s = pstring s .>> ws
@@ -48,7 +48,7 @@ let pFilter =
   let pColValue =
     let normalChar = satisfy (fun c -> c <> '\"')    
     between (pstring "\"") (pstring "\"") (many1Chars normalChar)  
-  let pBody = pipe2 (pColName .>> ws) (pstring "=" >>. ws >>. pColValue) (fun x  z -> Filter (x, z))
+  let pBody = pipe2 (pColName .>> ws) (pstring "=" >>. ws >>. pColValue) (fun x  z -> FilterExpression (x, z))
   str_ws "filter" >>. str_ws "(" >>.  pBody .>> str_ws ")"
 
 test pFilter "filter([専門] = \"数学\")"
