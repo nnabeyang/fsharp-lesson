037e67bc8c7c37b5eb3536524fa69ee620d2141a
nnabeyang <nabeyang@gmail.com>
2022-08-20 10:14:10 +0900

    課題11

--- sources/play_library/PlayFParsec/Scratch.fsx
+++ sources/play_library/PlayFParsec/Scratch.fsx
@@ -23,3 +23,15 @@ let pidentifier  =
 test pidentifier "abc"
 test pidentifier "i18n"
 test pidentifier "12"
+
+// 課題11
+type Project = Project of string list
+let pProject =
+  let ws = spaces
+  let str_ws s = pstring s .>> ws
+  let column_ws = pColumn .>> ws
+  let pColumns = str_ws "(" >>. sepBy column_ws (str_ws ",") .>> str_ws ")"
+  str_ws "project" >>. pColumns |>> Project .>> ws
+
+test pProject "project([場所],  [学年])"
+test pProject "project ([場所][学年]"
