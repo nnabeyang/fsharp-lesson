3dd089a96b1e7fd71c857de986ad6dd27baca8b9
nnabeyang <nabeyang@gmail.com>
2022-08-26 08:21:37 +0900

    fix: evalXXXメソッドのevalはmodule名をかぶるため、名称を変更

--- sources/ToyRel/Eval.fs
+++ sources/ToyRel/Eval.fs
@@ -10,45 +10,45 @@ let randomString length =
   String.init length (fun _ -> char (random.Next( (int 'a'), (int 'z') + 1)) |> sprintf "%c")
 let randomBaseName() = "zz" + (randomString 4) |> Identifier.Identifier
 
-let rec evalExpression expr =
+let rec expression expr =
   match expr with
-    | Project cols -> evalProjectExpression cols
+    | Project cols -> projectExpression cols
     | Expression.Identifier basename -> Relation.readCsv basename
-and  evalProjectExpression (expr : ProjectExpression) =
+and  projectExpression (expr : ProjectExpression) =
   let (ident, cols) = expr
   let df = match ident with
-    | Project _ -> evalExpression ident
-    | _ -> evalExpression ident
+    | Project _ -> expression ident
+    | _ -> expression ident
   Relation.project cols df
 
 // 指定したRelationの内容を標準出力する
-let evalPrint ident =
+let printStatement ident =
   let df =
     Relation.readCsv ident
     |> Relation.toFrame
   df.Print()
 
 // データベース内のRelationの一覧を標準出力する
-let evalList() =
+let listStatement() =
   let relationList = 
     Directory.GetFiles databaseDir
     |> Seq.map Path.GetFileNameWithoutExtension
   printfn "%s" (relationList  |> String.concat Environment.NewLine)
 
 // 指定した名称で式を評価して得たRelationを保存する
-let evalAssignment (assignment: Assignment) =
-  let (ident, expr) = assignment
+let assignmentStatement (assign: Assignment) =
+  let (ident, expr) = assign
   let (Identifier.Identifier relationName) = ident
-  Relation.save (evalExpression expr) ident
+  Relation.save (expression expr) ident
   printfn "Relation %s returned." relationName
 
 // ランダムな名称で式を評価して得たRelationを保存する
-let evalExprStatement expr = evalAssignment (randomBaseName(), expr)
+let expressionStatement expr = assignmentStatement (randomBaseName(), expr)
 
 // Statementを評価する
 let evalStatement stmt =
   match stmt with
-    | ExpressionStatement expr -> evalExprStatement expr
-    | PrintStatement ident -> evalPrint ident
-    | AssignmentStatement assignment -> evalAssignment assignment
-    | ListStatement -> evalList()
+    | ExpressionStatement expr -> expressionStatement expr
+    | PrintStatement ident -> printStatement ident
+    | AssignmentStatement assignment -> assignmentStatement assignment
+    | ListStatement -> listStatement()
